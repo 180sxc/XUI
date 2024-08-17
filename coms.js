41995011b397b5c62896a4163a088c1ecefd685e
180sxc <xcsplgk@gmail.com>
2024-08-17 13:11:50 +0100

    Update coms.js

--- coms.js
+++ coms.js
@@ -8,6 +8,7 @@ window.addEventListener('load', function() {
 function startUp () {
   var canvas = document.getElementById("canvas")
   const ctx = canvas.getContext('2d');
+  animate ();
 }
 window.addEventListener('resize', resizeCanvas);
 function resizeCanvas () {
@@ -17,4 +18,22 @@ function resizeCanvas () {
   let canvas = document.getElementById("canvas");
   canvas.style.height = screenHeight + "px";
   canvas.style.width = screenWidth + "px";
+}
+
+
+class Particles {
+  constructor (x, y, ctx) {
+    this.x = x;
+    this.y = y;
+    this.size = 5;
+    this.ctx = ctx;
+  }
+}
+var particleCount = 1500;
+var particles = [];
+function animate () {
+  var canvas = document.getElementById("canvas")
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  requestAnimationFrame(animate);
 }
