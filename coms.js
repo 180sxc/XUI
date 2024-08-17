efdbc497e2f607e2ac04cf9a00adfc2d2208fb4e
180sxc <xcsplgk@gmail.com>
2024-08-17 13:45:39 +0100

    Update coms.js

--- coms.js
+++ coms.js
@@ -8,6 +8,7 @@ window.addEventListener('load', function() {
 function startUp () {
   var canvas = document.getElementById("canvas")
   const ctx = canvas.getContext('2d');
+  spawnParticles ()
   animate ();
 }
 window.addEventListener('resize', resizeCanvas);
@@ -27,13 +28,38 @@ class Particles {
     this.y = y;
     this.size = 5;
     this.ctx = ctx;
+    this.dir = 0;
+    this.speed = 4;
+  }
+  spawn () {
+    this.ctx.fillStyle = "rgba(255,255,255,0.8)";
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    this.ctx.fill();
+  }
+  update () {
   }
 }
-var particleCount = 1500;
+var particleCount = 500;
 var particles = [];
+
+function spawnParticles () {
+  var canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext('2d');
+  for(let i = 0; i < particleCount; i++){
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    particles.push(new Particles(x, y, ctx));
+  }
+}
+
 function animate () {
   var canvas = document.getElementById("canvas")
   const ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particles.forEach(particle => {
+    particle.update();
+    particle.spawn();
+  });
   requestAnimationFrame(animate);
 }
