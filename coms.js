990b5b41a484b53e741d551a282900c411602968
180sxc <xcsplgk@gmail.com>
2024-08-17 12:24:36 +0100

    Update coms.js

--- coms.js
+++ coms.js
@@ -2,8 +2,17 @@ let htmlLoaded = false;
 window.addEventListener('load', function() {
   htmlLoaded = true;
   startUp();
+  resizeCanvas();
 })
 function startUp () {
   var canvas = document.getElementById("canvas")
   const ctx = canvas.getContext('2d');
+}
+window.addEventListener('resize', resizeCanvas());
+function resizeCanvas () {
+  let screenWidth = window.innerWidth;
+  let screenHeight = window.innerHeight;
+  let canvas = document.getElementById("canvas");
+  canvas.style.height = screenHeight + "px";
+  canvas.style.width = screenWidth + "px";
 }
